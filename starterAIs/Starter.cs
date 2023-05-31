2298f40eb0a924cd38a8bc2b98230fb6bff94b0a
Julien Poulton <julien@codingame.com>
2023-05-31 11:48:05 +0200

    keep only one c# starter

--- starterAIs/Starter.cs
+++ starterAIs/Starter.cs
@@ -1,31 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
 
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
 class Player
 {
     static void Main(string[] args)
     {
         string[] inputs;
         int numberOfCells = int.Parse(Console.ReadLine()); // amount of hexagonal cells in this map
-        var cells = new List<Cell>();
+
+        List<Cell> cells = new List<Cell>();
+
         for (int i = 0; i < numberOfCells; i++)
         {
             inputs = Console.ReadLine().Split(' ');
-            var cell = new Cell
-            {
-                Index = i,
-                Type = (CellType)int.Parse(inputs[0]), // 0 for empty, 1 for eggs, 2 for food
-                Resource = int.Parse(inputs[1]) // the initial amount of eggs/crystals on this cell
-            };
-            cell.AddNeighbor(int.Parse(inputs[2]), int.Parse(inputs[3]), int.Parse(inputs[4]), int.Parse(inputs[5]),
-                int.Parse(inputs[6]), int.Parse(inputs[7])); // the index of the neighbouring cell for each direction
+            int type = int.Parse(inputs[0]); // 0 for empty, 1 for eggs, 2 for crystal
+            int initialResources = int.Parse(inputs[1]); // the initial amount of eggs/crystals on this cell
+            int neigh0 = int.Parse(inputs[2]); // the index of the neighbouring cell for each direction
+            int neigh1 = int.Parse(inputs[3]);
+            int neigh2 = int.Parse(inputs[4]);
+            int neigh3 = int.Parse(inputs[5]);
+            int neigh4 = int.Parse(inputs[6]);
+            int neigh5 = int.Parse(inputs[7]);
+
+            Cell cell = new Cell();
+            cell.index = i;
+            cell.cell_type = type;
+            cell.ressources = initialResources;
+
+            cell.neighbors = new int[6] { neigh0, neigh1, neigh2, neigh3, neigh4, neigh5 };
+            
+            cell.my_ants = 0;
+            cell.opp_ants = 0;
+
             cells.Add(cell);
         }
-
         int numberOfBases = int.Parse(Console.ReadLine());
-        var myBaseIndices = Console.ReadLine().Split(' ').Take(numberOfBases).Select(int.Parse);
-        var oppBaseIndices = Console.ReadLine().Split(' ').Take(numberOfBases).Select(int.Parse);
+
+        List<int> my_bases = new List<int>();
+        List<int> opp_bases = new List<int>();
+
+        inputs = Console.ReadLine().Split(' ');
+        for (int i = 0; i < numberOfBases; i++)
+        {
+            int myBaseIndex = int.Parse(inputs[i]);
+            my_bases.Add(myBaseIndex);
+        }
+        inputs = Console.ReadLine().Split(' ');
+        for (int i = 0; i < numberOfBases; i++)
+        {
+            int oppBaseIndex = int.Parse(inputs[i]);
+            opp_bases.Add(oppBaseIndex);
+        }
 
         // game loop
         while (true)
@@ -33,54 +66,48 @@ class Player
             for (int i = 0; i < numberOfCells; i++)
             {
                 inputs = Console.ReadLine().Split(' ');
-                cells[i].Resource = int.Parse(inputs[0]); // the current amount of eggs/crystals on this cell
-                cells[i].MyAnts = int.Parse(inputs[1]); // the amount of your ants on this cell
-                cells[i].OppAnts = int.Parse(inputs[2]); // the amount of opponent ants on this cell
+                int resources = int.Parse(inputs[0]); // the current amount of eggs/crystals on this cell
+                int myAnts = int.Parse(inputs[1]); // the amount of your ants on this cell
+                int oppAnts = int.Parse(inputs[2]); // the amount of opponent ants on this cell
+
+                cells[i].ressources = resources;
+                cells[i].my_ants = myAnts;
+                cells[i].opp_ants = oppAnts;
             }
 
-            var actions = new List<string>();
             // Write an action using Console.WriteLine()
             // To debug: Console.Error.WriteLine("Debug messages...");
+            List<string> actions = new List<string>();
 
-            // From here, add the actions you want to the list, e.g.
-            // actions.add("LINE 1 5 1")
-            // actions.add("BEACON 1 5")
+
+            // ------- ! -----   Now you can generate a list of actions to do --------
+            // WAIT | LINE <sourceIdx> <targetIdx> <strength> | BEACON <cellIdx> <strength> | MESSAGE <text>
+
+
+            // actions.Add("LINE 5 21 1");   
+
 
             // WAIT | LINE <sourceIdx> <targetIdx> <strength> | BEACON <cellIdx> <strength> | MESSAGE <text>
-            if (!actions.Any())
+            if (actions.Count == 0)
             {
                 Console.WriteLine("WAIT");
             }
             else
             {
-                // Print all actions
-                Console.WriteLine(string.Join(';', actions));
+                string final_action = String.Join(";", actions);
+                Console.WriteLine(final_action);
             }
+
         }
     }
-}
 
-enum CellType
-{
-    Empty = 0,
-    Egg = 1,
-    Crystal = 2
-}
-
-class Cell
-{
-    public int Index { get; set; }
-    public CellType Type { get; set; }
-    public int Resource { get; set; }
-    public IList<int> Neighbors { get; set; } = new List<int>();
-    public int MyAnts { get; set; }
-    public int OppAnts { get; set; }
-
-    public void AddNeighbor(params int[] neighborIndices)
+    public class Cell
     {
-        foreach (var neighborIndex in neighborIndices.Where(n => n > -1))
-        {
-            Neighbors.Add(neighborIndex);
-        }
+        public int index { get; set; }
+        public int cell_type { get; set; }
+        public int ressources { get; set; }
+        public int[]? neighbors { get; set; }
+        public int my_ants { get; set; }
+        public int opp_ants { get; set; }
     }
 }
